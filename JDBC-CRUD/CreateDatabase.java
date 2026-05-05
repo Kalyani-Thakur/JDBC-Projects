b6d725940f49f1ea23d2ee694e25403d76b70dc0
Kalyani Thakur <kalyanimdb2001@gmail.com>
2026-05-05 21:06:18 +0530

    Implement database creation in CreateDatabase.java
    This program connects to a MySQL database and creates a new database named 'mydatabase'.

--- JDBC-CRUD/CreateDatabase.java
+++ JDBC-CRUD/CreateDatabase.java
@@ -1,1 +1,33 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class CreateDatabase {
+    public static void main(String[] args) {
+    //  Connecting database  
+
+        // conn establish
+        // statement create
+        // execute - query
+        // conn close
+
+        try {
+            String url = "jdbc:mysql://localhost:3306/";
+            String userName = "YourUsername";
+            String password = "YourPassword";
+
+            Connection conn = DriverManager.getConnection(url, userName, password); 
+            Statement stm = conn.createStatement();
+
+            String query = "Create database mydatabase";
+            boolean bl = stm.execute(query);
+
+            System.out.println("Database created successfully = " +bl);
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
 
